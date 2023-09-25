dce5d2e084fc671aa227aa9c294f52dd7b85606f
maveriq <harisjabbar@gmail.com>
2023-09-25 23:23:23 +0200

    zeroshot results

--- zeroshot-openai.py
+++ zeroshot-openai.py
@@ -4,6 +4,7 @@ from datasets import load_from_disk
 from templates import template1
 from functools import partial
 import pickle
+from tqdm import tqdm
 
 dataset = load_from_disk('goemotion_subset')
 dataset = dataset['validation']
@@ -12,14 +13,14 @@ openai.api_key = os.environ['OPENAI']
 
 template = partial(template1,eval=True)
 prompted = dataset.map(template,remove_columns=dataset.column_names)
-
+prompted = prompted.select(range(100))
 print(prompted)
 
 instruction = "Select one or more emotions in the following sentence from this list of emotions : 'anger', 'disgust', 'fear', 'joy', 'sadness', 'surprise'."
 
 results = []
 responses = []
-for sample in prompted.select(range(100)):
+for sample in tqdm(prompted):
     prompt = instruction + sample['prompt']
     response = openai.Completion.create(model="gpt-3.5-turbo-instruct",prompt = prompt, max_tokens = 16)
 
